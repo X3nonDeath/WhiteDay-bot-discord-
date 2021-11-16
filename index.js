e07c189688248f5a23afe1e0c7a3f6b28ea87aec
X3nonDeath <94478499+X3nonDeath@users.noreply.github.com>
2021-11-16 19:25:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require("dotenv").config();
+
+client.on("ready", () => {
+    console.log("I'm ready !");
+});
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
